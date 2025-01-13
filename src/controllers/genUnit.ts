1913da5dccfa83400eed29adc282cb95857fe629
nikrr4000 <romanovnr20@gmail.com>
2025-01-13 17:07:26 +0300

    fix

--- src/controllers/genUnit.ts
+++ src/controllers/genUnit.ts
@@ -44,12 +44,13 @@ export default (ctx: MyContext, ...args: basicCallbackArgs) => ({
 		const records = await recordsCtrl.findFutureRecords({ userId })
 
 		let text = 'На данный момент вы записаны на следующие приемы:\n\n'
-		text += records.map(record => {
+		text += records.map(async record => {
 			const apptDate = dates.parseApptDate(record.Appointment.start)
 			const apptDateDay = dates.getStrDateWithoutTime(apptDate)
 			const slotInterval = `${record.start} - ${record.end}`
-
-			createRecordTexts.basicText(record.Appointment.place, apptDateDay, record.Procedure.name, slotInterval)
+			const appointment = await apptCtrl.findOne({ id: record.apptId })
+			const checkedAppt = appointment?.place || 'Свяжитесь с @darialalala'
+			createRecordTexts.basicText(checkedAppt, apptDateDay, record.Procedure.name, slotInterval)
 		}).join('\n\n')
 		const k = createRecordKs.basic(records, false).row().text('Назад', 'back')
 
